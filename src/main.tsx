0c96812b228814bcb330a8c181da6864454a568a
SergeP <serge.prysiazhnyi@gmail.com>
2023-03-15 11:01:57 +0200

    feat: add types, constants, helpers, apiService

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,8 +6,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+type Suits = 'HEARTS' | 'DIAMONDS' | 'CLUBS' | 'SPADES';
+
+export interface CardItem {
+  code: string;
+  image: string;
+  images: {
+    png: string;
+    svg: string;
+  };
+  suit: Suits;
+  value: string;
+}
+
+export interface AppState {
+  deckId: string;
+  previousCard: CardItem;
+  currentCard: CardItem;
+  isLoading: boolean;
+  error: unknown;
+  hasSuitMatch: boolean;
+  hasValueMatch: boolean;
+  remaining: number;
+  valueMatches: number;
+  suitMatches: number;
+}
+
+interface FetchBaseResponse {
+  success: boolean;
+  remaining: number;
+  deck_id: string;
+}
+
+export interface FetchDeckResponse extends FetchBaseResponse {
+  shuffled: boolean;
+}
+
+export interface FetchCardsResponse extends FetchBaseResponse {
+  cards: CardItem[];
+}
