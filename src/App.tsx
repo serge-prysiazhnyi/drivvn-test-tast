2c2c3dcb46e4f4026ab2c5f2a05a553fefd9c51a
SergeP <serge.prysiazhnyi@gmail.com>
2023-03-15 11:04:02 +0200

    feat: add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,124 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 
-import { Button } from './components/button';
-import logo from './logo.svg';
+import { fetchData } from './apiService';
+import { Button } from './components/Button';
+import { Cards } from './components/Cards';
+import { Match } from './components/Match';
+import { Result } from './components/Result';
+import { BASE_URL } from './constants';
+import { getErrorMessage } from './helpers';
+import { AppState, FetchCardsResponse, FetchDeckResponse } from './types';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState<AppState>({
+    deckId: null,
+    previousCard: null,
+    currentCard: null,
+    isLoading: false,
+    error: null,
+    hasSuitMatch: false,
+    hasValueMatch: false,
+    remaining: 52,
+    valueMatches: 0,
+    suitMatches: 0,
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setState((prevState) => ({
+          ...prevState,
+          isLoading: true,
+        }));
+        const res: FetchDeckResponse = await fetchData(
+          `${BASE_URL}/new/shuffle/?deck_count=1`,
+        );
+        setState((prevState) => ({
+          ...prevState,
+          deckId: res.deck_id,
+          isLoading: false,
+        }));
+      } catch (e) {
+        setState((prevState) => ({
+          ...prevState,
+          isLoading: false,
+          error: getErrorMessage(e),
+        }));
+      }
+    };
+    getData();
+  }, []);
+
+  const handleGetCard = useCallback(async () => {
+    try {
+      setState((prevState) => ({
+        ...prevState,
+        isLoading: true,
+      }));
+
+      const res: FetchCardsResponse = await fetchData(
+        `${BASE_URL}/${state.deckId}/draw/?count=1`,
+      );
+
+      if (res.success) {
+        setState((prevState) => {
+          const newCurrent = res.cards[0];
+          const hasSuitMatch = prevState.currentCard?.suit === newCurrent.suit;
+          const hasValueMatch = prevState.currentCard?.value === newCurrent.value;
+
+          return {
+            ...prevState,
+            previousCard: prevState.currentCard,
+            currentCard: newCurrent,
+            isLoading: false,
+            hasSuitMatch,
+            hasValueMatch,
+            valueMatches: hasValueMatch
+              ? prevState.valueMatches + 1
+              : prevState.valueMatches,
+            suitMatches: hasSuitMatch ? prevState.suitMatches + 1 : prevState.suitMatches,
+            remaining: res.remaining,
+          };
+        });
+      }
+    } catch (e) {
+      setState((prevState) => ({
+        ...prevState,
+        isLoading: false,
+        error: getErrorMessage(e),
+      }));
+    }
+  }, [state.deckId]);
+
+  const handleClearError = useCallback(() => {
+    setState((prevState) => ({
+      ...prevState,
+      error: null,
+    }));
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-        <Button />
-      </header>
-    </div>
+    <Container className={state.isLoading ? 'AppLoading my-3' : 'my-3'}>
+      {/* FIX SHIFT */}
+      {state.remaining > 0 && (
+        <Match hasSuitMatch={state.hasSuitMatch} hasValueMatch={state.hasValueMatch} />
+      )}
+      <Cards cards={[state.previousCard, state.currentCard]} />
+      {state.remaining > 0 ? (
+        <Button onClick={handleGetCard} disabled={state.isLoading} />
+      ) : (
+        <Result valueMatches={state.valueMatches} suitMatches={state.suitMatches} />
+      )}
+      {state.error && (
+        <Alert variant="danger" onClose={handleClearError} className="mt-3" dismissible>
+          {state.error}
+        </Alert>
+      )}
+    </Container>
   );
 }
 
